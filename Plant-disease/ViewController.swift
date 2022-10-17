3801a9ed53e134bc18c67da2b9a684182165772a
Nurillo <nurillod23@mail.ru>
2022-10-17 08:58:43 +0500

    Initial Commit

---
+++ Plant-disease/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Plant-disease
+//
+//  Created by Nurillo Domlajonov on 17/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
